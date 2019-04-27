c4e8d89660970d799edfc507ff2087ae237e2226
Alezcano728 <46847654+Alezcano728@users.noreply.github.com>
2019-04-26 20:07:39 -0600

    Update UsuarioManager.cs

--- Proyecto_Xamarin/Proyecto_Xamarin/Managers/UsuarioManager.cs
+++ Proyecto_Xamarin/Proyecto_Xamarin/Managers/UsuarioManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,18 +9,9 @@ namespace Proyecto_Xamarin.Managers
 {
     public class UsuarioManager
     {
-        //const string UrlRegister = "http://localhost:56485/api/login/Register/";
-        //const string URLUpdate = "http://localhost:56485/api/login/Update/";
-        //const string URLTraerPagosUser = "http://localhost:56485/api/login/";
-        //const string URLTraerInformacionUser = "http://localhost:56485/api/login/Informacion/";
 
-        const string UrlAuthenticate = "http://192.168.0.7:45455/api/login/authenticate/";
-        const string UrlRegister = "http://192.168.0.7:45455/api/login/Register/";
-        const string URLUpdate = "http://192.168.0.7:45455/api/login/Update/";
-        const string URLTraerPagosUser = "http://192.168.0.7:45455/api/login/";
-        const string URLTraerInformacionUser = "http://192.168.0.7:45455/api/login/Informacion/";
-
-
+        //const string UrlAuthenticate = "http://192.168.0.7:45455/api/login/authenticate/";
+        const string UrlAuthenticate = "https://www.gruposama.com/WebApiSecureSAMA/api/LoginExt/authenticate";
        
 
         public async Task<Usuario> Validar(string username, string password)
@@ -57,19 +48,5 @@ namespace Proyecto_Xamarin.Managers
             }
         }
 
-        public async Task<Usuario> Registrar(Usuario usuario)
-        {
-
-            HttpClient client = new HttpClient();
-            var response = await client.PostAsync(UrlRegister,
-                new StringContent(JsonConvert.SerializeObject(usuario),
-                Encoding.UTF8, "application/json"));
-
-            return JsonConvert.DeserializeObject<Usuario>(await response.Content.ReadAsStringAsync());
-        }
-
-        public UsuarioManager()
-        {
-        }
     }
 }
